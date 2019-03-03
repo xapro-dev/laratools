deee8817dde4e508a332bb29a7cca500af49e438
xapro <xapro.dev@gmail.com>
2019-03-04 00:04:59 +0300

    fixed

--- src/Console/AddUser.php
+++ src/Console/AddUser.php
@@ -14,15 +14,26 @@ class AddUser extends Command
     protected $email;
     protected $password;
 
-    protected $signature = 'xapro:adduser {-A|admin}';
+    protected $signature = 'xapro:adduser {--A|admin}';
 
     protected $description = 'Add new user to users table';
 
     public function handle()
     {
         $this->name = $this->ask('Set user\'s name (empty by default).') ?? '';
-        $this->email = $this->ask('Set user\'s email (empty by default).') ?? '';
-        $this->password = $this->ask('Set user\'s name (empty by default).') ?? '';
+        
+        $this->email = $this->ask('Set user\'s email (empty by default).') 
+            ?? ($this->name 
+                ? $this->name . '@' . substr(config('app.url'), strpos(config('app.url'), '://') + 3) 
+                : null);
+        
+        if (!$this->email) return $this->error('lol?');
+        
+        $this->password = $this->ask('Set user\'s password (empty by default).') ?? '';
+        
+        $this->role = $this->ask('Set user\'s role (empty by default).') ?? '';
+        
+        
         $user = User::make(
             [
                 'name' => $this->name,
@@ -33,6 +44,6 @@ class AddUser extends Command
 
         $headers = ['name','email','password'];
 
-        $this->table($headers, $user->only(['name','email','password']));
+        $this->table($headers, [$user->only(['name','email','password'])]);
     }
 }
--- src/LaratoolsServiceProvider.php
+++ src/LaratoolsServiceProvider.php
@@ -13,21 +13,8 @@ use Xapro\Laratools\Console\AddUser;
 
 class LaratoolsServiceProvider extends ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
     public function register()
     {
-        die('qwe');
         $this->commands(
             [AddUser::class]
         );
